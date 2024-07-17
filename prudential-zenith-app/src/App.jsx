79dc4c387b992ff90a400a820a85e7cbd94b3482
DEV-SHEGZ <strexy1998@gmail.com>
2024-07-17 23:26:52 +0100

    adjusted the buttons with css to fit design

--- prudential-zenith-app/src/App.jsx
+++ prudential-zenith-app/src/App.jsx
@@ -45,7 +45,7 @@ export default function NavBar() {
 
       <div class=" row">
         <div class="col-1"></div>
-        <ul class="col-10  d-flex justify-content-around bg-danger pt-5 pb-5 mt-3 rounded-start rounded-end">
+        <ul class="col-10  d-flex justify-content-around  pt-4 pb-4 mt-3 rounded-start rounded-end low-nav">
           <li>Life stimulator</li>
           <li>Buy a policy</li>
           <li>Make a claim</li>
