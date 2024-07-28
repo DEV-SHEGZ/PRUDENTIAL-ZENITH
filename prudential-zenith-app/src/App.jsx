9dc320510a963a71f0861af4701731a678c98428
DEV-SHEGZ <strexy1998@gmail.com>
2024-07-28 04:42:48 +0100

    major changes

--- prudential-zenith-app/src/App.jsx
+++ prudential-zenith-app/src/App.jsx
@@ -7,45 +7,241 @@ import { CiCreditCard1 } from "react-icons/ci";
 export default function NavBar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-light row txt">
-        <div className="col-1"></div>
-        <div class="container col-10">
+      <nav class="navbar navbar-expand-lg ">
+        <div class="container">
+          <img class="image" src={img} alt="" />
+          <div class="dissapear">
+            <div class=" d-flex align-items-center">
+              <HiMagnifyingGlass className="mag" />
+              <TiMessages className="msg" />
+              <div class="d-flex align-items-center ">
+                <CiCreditCard1 className="crd" />
+                Online Payments
+                <div class="d-flex " className="click">
+                  <a class="indv" href="">
+                    Individual
+                  </a>
+                  <a href="co-p">Cooperate</a>
+                </div>
+                <button>Login</button>
+              </div>
+            </div>
+          </div>
+
           <button
-            class="navbar-toggler"
+            class="navbar-toggler collapsed"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
             aria-label="Toggle navigation"
           >
             <span class="navbar-toggler-icon"></span>
           </button>
-
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <img src={img} />
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-            <div class="d-flex align-items-center" role="search">
-              <HiMagnifyingGlass className="mag" />
-              <TiMessages className="msg" />
-              <CiCreditCard1 className="crd" />
-              Online Payments
-              <div class="d-flex " className="click">
-                <a class="indv" href="">
-                  Individual
-                </a>
-                <a href="">Cooperate</a>
-              </div>
-              <button>Login</button>
-            </div>
-          </div>
         </div>
-        <div className="col-1"></div>
       </nav>
 
-      <div class=" row">
+      <div
+        class="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+            Offcanvas
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <div class="btn-group">
+              <button
+                type="button"
+                class="btn btn-danger dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Action
+              </button>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider"></hr>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="btn-group">
+              <button
+                type="button"
+                class="btn btn-danger dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Action
+              </button>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider"></hr>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="btn-group">
+              <button
+                type="button"
+                class="btn btn-danger dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Action
+              </button>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider"></hr>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="btn-group">
+              <button
+                type="button"
+                class="btn btn-danger dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Action
+              </button>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider"></hr>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="btn-group">
+              <button
+                type="button"
+                class="btn btn-danger dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Action
+              </button>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider"></hr>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </ul>
+        </div>
+      </div>
+
+      <div class="dissapear row">
         <div class="col-1"></div>
-        <div class=" navbar col-10   pt-4 pb-4 mt-3 rounded-start rounded-end low-nav">
+        <div class=" navbar col-10  d-flex justify-content-evenly  ms-2  pt-4 pb-4 mt-3 rounded-start rounded-end low-nav">
           <li>Life stimulator</li>
           <li>Buy a policy</li>
           <li>Make a claim</li>
